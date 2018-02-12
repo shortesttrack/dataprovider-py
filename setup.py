01bb8cb03b4edbb7c581a4c67d5c9ac7a3276e79
quanself <446513370@qq.com>
2018-02-12 22:22:43 +0800

    modify version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 
 from setuptools import setup
 
-version = '1.0.0'
+version = '1.0.1'
 
 setup(
   name='st-dataprovider',
@@ -29,7 +29,7 @@ setup(
   author='Fugui Xing',
   author_email='admin@shortesttrack.com',
   url='https://github.com/shortesttrack/dataprovider-py',
-  download_url='https://github.com/shortesttrack/dataprovider-py/archive/v1.0.0.zip',
+  download_url='https://github.com/shortesttrack/dataprovider-py/archive/v1.0.1.zip',
   keywords=[
     'Shortest Track',
     'st_library',
