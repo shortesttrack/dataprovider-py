c8a8ea5837998c371a20ea2d2338ec8e3d86214f
Fugui Xing <446513370@qq.com>
2018-02-14 20:54:54 -0800

    fix the path of data in the example of upload_data

--- samples/structured_data/upload_data.py
+++ samples/structured_data/upload_data.py
@@ -6,4 +6,4 @@ st_lib.set_token('Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI4MTIyYT
 st_lib.set_configuration_uuid('52db99d3-edfb-44c5-b97a-f09df4402081')
 
 tbl = stdata.Table("edac25ec-25e3-4949-9f49-d42124c26bf2", "52db99d3-edfb-44c5-b97a-f09df4402081", "bbb")
-print (tbl.upload_data(r"/home/st/workspace/st-dataprovider-python/datalab/structured_data/data.csv"))
+print (tbl.upload_data(r"/data/structured_data/data.csv"))
