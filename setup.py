400ce6cd6372354a04270c3632e8c33eb53565c7
Fugui Xing <446513370@qq.com>
2018-02-19 05:16:30 -0800

    add batch API and streaming API of SEC

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 
 from setuptools import setup
 
-version = '1.0.6'
+version = '1.0.7'
 
 setup(
   name='st-dataprovider',
@@ -29,7 +29,7 @@ setup(
   author='Fugui Xing',
   author_email='admin@shortesttrack.com',
   url='https://github.com/shortesttrack/dataprovider-py',
-  download_url='https://github.com/shortesttrack/dataprovider-py/archive/v1.0..zip',
+  download_url='https://github.com/shortesttrack/dataprovider-py/archive/v1.0.7.zip',
   keywords=[
     'Shortest Track',
     'st_library',
